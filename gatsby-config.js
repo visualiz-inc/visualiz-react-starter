4d2ba40e9a4484c1b733023a1def5a528ee44562
rempei-okada <renpei.okada@outlook.com>
2021-10-22 07:31:49 +0900

    Update package

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,44 +8,48 @@
 const clientRoutes = ["/app/*"];
 
 module.exports = {
-  // pathPrefix: `/path`, github pagesなどを利用する場合はこちらにルートパスを指定
-  siteMetadata: {
-    title: "",
-  },
-  plugins: [
-    `gatsby-plugin-resolve-src`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-material-ui`,
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-emotion`,
-      options: {
-        // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
-        // The values for each key in this example are the defaults the plugin uses.
-        sourceMap: true,
-        autoLabel: "dev-only",
-        labelFormat: `[local]`,
-        cssPropOptimization: true,
-      }
+    // pathPrefix: `/path`, github pagesなどを利用する場合はこちらにルートパスを指定
+    siteMetadata: {
+        title: "",
     },
-    {
-      resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [...clientRoutes] },
-    },
-    {
-      resolve: "gatsby-plugin-eslint",
-      options: {
-        stages: ["develop"],
-        extensions: ["js", "jsx", "ts", "tsx"],
-        exclude: ["node_modules", ".cache", "public"],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-page-creator`,
-      options: {
-        path: `${__dirname}/src/StaticPages/Endpoints`,
-      },
-    },
-  ],
+    plugins: [
+        `gatsby-plugin-resolve-src`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-material-ui`,
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-webpack-bundle-analyser-v2`,
+        `gatsby-plugin-image`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-plugin-emotion`,
+            options: {
+                // Accepts the following options, all of which are defined by `@emotion/babel-plugin` plugin.
+                // The values for each key in this example are the defaults the plugin uses.
+                sourceMap: true,
+                autoLabel: "dev-only",
+                labelFormat: `[local]`,
+                cssPropOptimization: true,
+            }
+        },
+        {
+            resolve: `gatsby-plugin-create-client-paths`,
+            options: { prefixes: [...clientRoutes] },
+        },
+        {
+            resolve: "gatsby-plugin-eslint",
+            options: {
+                stages: ["develop"],
+                extensions: ["js", "jsx", "ts", "tsx"],
+                exclude: ["node_modules", ".cache", "public"],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-page-creator`,
+            options: {
+                path: `${__dirname}/src/StaticPages/Endpoints`,
+            },
+        },
+    ],
 };
